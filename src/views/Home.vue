58895466ea1b74477b1eba50abcbc09ea7c0b245
Cintiya Dewiani <dewianic@gmail.com>
2021-04-10 09:01:09 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <Instagram />
+    <PartnerLogo />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import Instagram from "@/components/Instagram.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    Instagram,
+    PartnerLogo,
+    FooterShayna,
+  },
+};
+</script>
